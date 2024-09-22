8731c36c38ef8e05de423023bfaa4f8ee86c9647
gabrielbarth <gabriel.silverio@stone.com.br>
2024-09-21 23:08:24 -0400

    feat: add EventElements const

---
+++ src/utils/constants/eventElements.ts
@@ -0,0 +1,15 @@
+import { EventElement } from "../../types/event/EventElement";
+
+export const eventElements = [
+  EventElement.Page,
+  EventElement.Button,
+  EventElement.Image,
+  EventElement.List,
+  EventElement.Item,
+  EventElement.Card,
+  EventElement.Link,
+  EventElement.Text,
+  EventElement.Modal,
+  EventElement.Snackbar,
+  EventElement.Toast,
+] as const;
