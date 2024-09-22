7cd83b3c98151283b5ed65359da4d0a316f3df71
gabrielbarth <gabriel.silverio@stone.com.br>
2024-09-21 23:07:59 -0400

    feat: add EventElement type

---
+++ src/types/event/EventElement.ts
@@ -0,0 +1,13 @@
+export enum EventElement {
+  Page = "page",
+  Button = "button",
+  Image = "image",
+  List = "list",
+  Item = "item",
+  Card = "card",
+  Link = "link",
+  Text = "text",
+  Modal = "modal",
+  Snackbar = "snackbar",
+  Toast = "toast",
+}
