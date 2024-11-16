35cba227583b5db17b6ab59fd8d6cf2a97b7ac72
gabrielbarth <gabriel.silverio@stone.com.br>
2024-11-15 23:37:29 -0400

    ref: remove example and improve types

--- src/features/eventCreator/index.ts
+++ src/features/eventCreator/index.ts
@@ -1,16 +1,17 @@
-import { EventReturn } from "../../types/event/EventHandlerReturn";
+import { EventElement } from "../../types/event/EventElement";
 import { EventHandler } from "../../types/event/EventHandlers";
 import { EventMetadata } from "../../types/event/EventMetadata";
 import { eventActions } from "../../utils/constants/eventActions";
 import { eventElements } from "../../utils/constants/eventElements";
 
 // function to create an object structure [element][action] and returns callback
-function eventCreator(callback: (event: any) => EventReturn): EventHandler {
+export function eventCreator(
+  callback: (cbEventMetadata: unknown) => unknown
+): EventHandler {
   const handlers: EventHandler = {} as EventHandler;
-
   // iterate over each element
   eventElements.forEach((element) => {
-    handlers[element] = {};
+    handlers[element] = {} as EventHandler[EventElement];
 
     // iterate over each action
     eventActions.forEach((action) => {
@@ -23,26 +24,3 @@ function eventCreator(callback: (event: any) => EventReturn): EventHandler {
 
   return handlers;
 }
-
-function logEvent(data: any): any {
-  console.log(
-    "-------------EVENT LOG-------------\n",
-    JSON.stringify(data, null, 2),
-    "\n-------------END OF EVENT LOG-------------\n"
-  );
-}
-
-// usage example
-const eventHandlers = eventCreator(logEvent);
-
-eventHandlers.button.click({
-  id: "btn-1",
-  timestamp: Date.now(),
-  description: "Button clicked",
-});
-
-eventHandlers.page.view({
-  id: "page-1",
-  timestamp: Date.now(),
-  description: "Page viewed",
-});
