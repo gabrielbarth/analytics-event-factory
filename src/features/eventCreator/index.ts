9efbbc01b8f6fe5c74aebb8c63eeb2c36559a792
gabrielbarth <gabriel.silverio@stone.com.br>
2025-02-08 20:28:45 -0400

    feat: set page option on event label and metadata

--- src/features/eventCreator/index.ts
+++ src/features/eventCreator/index.ts
@@ -1,3 +1,4 @@
+import { EventCreatorParams } from "../../types/event/EventCreatorParams";
 import { EventElement } from "../../types/event/EventElement";
 import { EventHandler } from "../../types/event/EventHandlers";
 import { EventMetadata } from "../../types/event/EventMetadata";
@@ -5,10 +6,17 @@ import { eventActions } from "../../utils/constants/eventActions";
 import { eventElements } from "../../utils/constants/eventElements";
 
 // function to create an object structure [element][action] and returns callback
-export function eventCreator(
-  callback: (cbEventMetadata: unknown) => unknown
-): EventHandler {
+export function eventCreator({
+  callback,
+  options,
+}: EventCreatorParams): EventHandler {
   const handlers: EventHandler = {} as EventHandler;
+
+  const page = options.page.showOnLabel
+    ? `${options.page.currentPage.trim()}.`
+    : "";
+  const showPageOnMetadata = options.page.showOnMetadata;
+
   // iterate over each element
   eventElements.forEach((element) => {
     handlers[element] = {} as EventHandler[EventElement];
@@ -17,7 +25,13 @@ export function eventCreator(
     eventActions.forEach((action) => {
       // define an action handler that receives event metadata
       handlers[element][action] = (eventMetadata: EventMetadata) => {
-        callback({ label: `${element}.${action}`, metadata: eventMetadata });
+        callback({
+          label: `${page}${element}.${action}`,
+          metadata: {
+            ...eventMetadata,
+            ...(showPageOnMetadata ? { page: page.slice(0, -1) } : {}),
+          },
+        });
       };
     });
   });
