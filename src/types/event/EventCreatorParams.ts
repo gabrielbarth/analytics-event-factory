3489147537980ae6511075893885ca588bd57771
gabrielbarth <gabriel.silverio@stone.com.br>
2025-02-08 20:26:17 -0400

    feat: add EventCreatorParams type

---
+++ src/types/event/EventCreatorParams.ts
@@ -0,0 +1,36 @@
+type StringFormat =
+  | "camelCase"
+  | "pascalCase"
+  | "snakeCase"
+  | "dotCase"
+  | "kebabCase"
+  | "noCase";
+
+type StringCase = "lowercase" | "uppercase";
+
+type EventCreatorParamsOptions = {
+  page?: {
+    currentPage?: string;
+    stringCase?: StringCase;
+    stringFormat?: StringFormat;
+    showOnLabel?: boolean;
+    showOnMetadata?: boolean;
+  };
+  action?: {
+    possibleActions?: string[];
+    stringCase?: StringCase;
+    stringFormat?: StringFormat;
+    showOnMetadata?: boolean;
+  };
+  element?: {
+    possibleElements?: string[];
+    stringCase?: StringCase;
+    stringFormat?: StringFormat;
+    showOnMetadata?: boolean;
+  };
+};
+
+export interface EventCreatorParams {
+  callback: (cbEventMetadata: unknown) => unknown;
+  options?: EventCreatorParamsOptions;
+}
